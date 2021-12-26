f6d07725c041dfdefe12377a37f55f35b112533e
Mehmet Hazar Artuner <hazar.artuner@gmail.com>
2021-12-26 22:12:26 +0300

    feat(app): implement react-app-rewired to the apps

---
+++ packages/presale/config-overrides.js
@@ -0,0 +1,6 @@
+const { override, addBabelPlugin } = require('customize-cra');
+
+module.exports = override(
+  addBabelPlugin('@babel/plugin-proposal-optional-chaining'),
+  addBabelPlugin('babel-plugin-styled-components')
+);
